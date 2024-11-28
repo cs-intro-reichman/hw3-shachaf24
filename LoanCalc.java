9c518a2d7bdf905349a2295cf8738284bd9a347a
Shachaf Amar <shachafmeir.amar@post.runi.ac.il>
2024-11-28 20:00:12 +0200

    2 complete

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,11 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		for(int i = 0; i < n; i++) {
+			loan = (loan - payment) * (1 + rate / 100);
+			
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +41,18 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		iterationCounter++;
+		double payment = loan/n;
+		while (Math.abs(payment) > epsilon) {
+			double balance = endBalance(loan, rate, n, payment);
+			payment += epsilon;
+			if (balance <= epsilon) {
+				return payment;
+			}
+		    iterationCounter++;
+		}
+
+		return payment;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +61,35 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+        double minPay = 0; 
+		double fullPay = loan; 
+		double estiPay = ( minPay + fullPay) / 2; 
+		
+		double remPay = loan;
+		iterationCounter = 0; 
+		
+		while (fullPay - minPay > epsilon) {
+			remPay = loan;
+
+			for (int i = 0; i < n; i++) {
+				remPay = (remPay - estiPay) * (1 + rate / 100);
+
+
+		}
+			
+
+		if (remPay > 0) {
+			minPay = estiPay;
+		}else{
+			fullPay = estiPay; 
+		}
+		
+		estiPay = (minPay + fullPay) / 2;
+		
+		iterationCounter++;
+		
     }
-}
+
+	return estiPay;
+	}
+}	
